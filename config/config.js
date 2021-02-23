2c45e3c572b8625c833ef7f29750e5a6e9fc6982
Sagar Saini <chasesagar@gmail.com>
2021-02-23 20:36:31 +0530

    demo config data

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
     env: 'development', // enviroment setting
     port: 5000, // port for running of server side applicatin
-    jwtSecret: "chaseJwtSecret@2021", // jwt secret for etting up JWT token
-    mongoUri: 'mongodb+srv://chasesagar-database:6iXMMxzPWBZGZQ7@cluster0.ihgyv.mongodb.net/chase-auth?retryWrites=true&w=majority' //  location of the MongoDB database instance for the project
+    jwtSecret: "Enter your jwt secret here", // jwt secret for etting up JWT token
+    mongoUri: 'mongodb+srv://<Cluser dbname>:<password>@cluster0.ihgyv.mongodb.net/<dbname>?retryWrites=true&w=majority' //  location of the MongoDB database instance for the project
 
 }
    
